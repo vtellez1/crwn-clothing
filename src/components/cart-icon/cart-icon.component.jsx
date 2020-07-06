5ab83085a6d083f45d611857d4af584d53122359
Vanessa <vanessatellez45@gmail.com>
2020-07-06 13:40:29 -0400

    updated app with reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect }  from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
@@ -18,8 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    itemCount: cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0)
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
 })
 
 export default connect(
