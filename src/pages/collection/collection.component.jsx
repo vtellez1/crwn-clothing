27831931f5175f614cb595085cf94b88feae1b08
Vanessa <vanessatellez45@gmail.com>
2020-07-07 14:29:20 -0400

    initial collection routing to be dynamic

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 import './collection.styles.scss';
+
 
 const CollectionPage = ({ match }) => (
     <div className='collection-page'>
@@ -10,4 +14,8 @@ const CollectionPage = ({ match }) => (
     </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
