51b1ddfe9b2419075a530be1d3b370d03bdddab0
Vanessa <vanessatellez45@gmail.com>
2020-07-08 21:46:14 -0400

    updated collection component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,11 +8,19 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return(
     <div className='collection-page'>
-        <h2>COLLECTION PAGE</h2>
+        <h2 className='title'>{ title }</h2>
+        <div className='items'>
+            {items.map(item => (
+                <CollectionItem key={item.id} item={item}/> 
+                ))}
+        </div>
     </div>
-);
+    );
+};
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
