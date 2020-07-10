8a3b978d6ff356b40ec3205dc7b5ad64debd8fb8
Vanessa <vanessatellez45@gmail.com>
2020-07-09 21:09:27 -0400

    edits

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -55,7 +55,7 @@ class SignUp extends React.Component {
         const { name, value } = event.target;
 
         this.setState({ [name]: value });
-    }
+    };
 
     render(){
         const { displayName, email, password, confirmPassword } = this.state;
