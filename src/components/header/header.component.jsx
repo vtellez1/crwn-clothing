d4c58b5ea0bda1ad8e05982689da82ba413e9520
Vanessa <vanessatellez45@gmail.com>
2020-07-28 12:55:56 -0400

    improved code to keep DRY for header styled component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -11,7 +10,7 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
  
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink} from './header.styles';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
     <HeaderContainer>
@@ -23,7 +22,7 @@ const Header = ({ currentUser, hidden }) => (
             <OptionLink to='/shop'>CONTACT</OptionLink>
             {
                 currentUser ?
-                <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+                <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
                 :
                 <OptionLink to='/signin'>SIGN IN</OptionLink>
             }
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,10 +1,5 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
-const OptionsContainerStyles = css`
-    padding: 10px 15px;
-    cursor: pointer;
-`
 
 export const HeaderContainer = styled.div`
     height: 70px;
@@ -29,9 +24,7 @@ export const OptionsContainer = styled.div`
 `
 
 export const OptionLink = styled(Link)`
-    ${OptionsContainerStyles}
+    padding: 10px 15px;
+    cursor: pointer;
 `
 
-export const OptionDiv = styled.div`
-    ${OptionsContainerStyles}
-`
