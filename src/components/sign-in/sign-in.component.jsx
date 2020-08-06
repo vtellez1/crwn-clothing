a2dbb4751c4aad2b649fff920cfc5ad2711adbc1
Vanessa <vanessatellez45@gmail.com>
2020-08-06 17:58:55 -0400

    refactored google sign in to saga

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { googleSignInStart } from '../../redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -38,6 +40,7 @@ class SignIn extends React.Component{
     };
 
     render(){
+        const { googleSignInStart } = this.props;
         return(
             <div className='sign-in'>
                 <h2>I already have an account</h2>
@@ -62,7 +65,7 @@ class SignIn extends React.Component{
 
                     <div className='buttons'>
                     <CustomButton type='submit'>Sign in</CustomButton>
-                    <CustomButton type='button' onClick={ signInWithGoogle } isGoogleSignIn>Sign in with Google</CustomButton>
+                    <CustomButton type='button' onClick={ googleSignInStart } isGoogleSignIn>Sign in with Google</CustomButton>
                     </div>
                 </form>
             </div>
@@ -70,4 +73,8 @@ class SignIn extends React.Component{
     }
 };
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart())
+})
+
+export default connect(null, mapDispatchToProps)(SignIn);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,35 @@
-import { UserActionTypes } from './user.types';
+import UserActionTypes from './user.types';
 
 export const setCurrentUser = user => ({
     type: UserActionTypes.SET_CURRENT_USER,
     payload: user
+});
+
+export const googleSignInStart = () => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_START
+});
+
+export const googleSignInFailure = user => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+    payload: user
+});
+
+export const googleSignInSuccess = error => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+    payload: error
+});
+
+export const emailSignInStart = emailAndPassword => ({
+    type: UserActionTypes.EMAIL_SIGN_IN_START,
+    payload: emailAndPassword
+});
+
+export const emailSignInFailure = user => ({
+    type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+    payload: user
+});
+
+export const emailSignInSuccess = error => ({
+    type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
+    payload: error
 });
