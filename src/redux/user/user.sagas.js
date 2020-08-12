47933468708220d5886ced46974eb87ac0e99dbe
Vanessa <vanessatellez45@gmail.com>
2020-08-12 17:10:04 -0400

    converted signout with sagas

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,7 +2,7 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import UserActionTypes from './user.types';
 
-import { signInSuccess, signInFailure } from './user.actions';
+import { signInSuccess, signInFailure, signOutSuccess, signOutFailure } from './user.actions';
 
 import { auth, googleProvider, createUserProfileDocument, getCurrentUser } from '../../firebase/firebase.utils';
 
@@ -44,6 +44,15 @@ export function* isUserAuthenticated(){
     }
 };
 
+export function* signOut(){
+    try{
+        yield auth.signOut();
+        yield put(signOutSuccess())
+    } catch (error){
+        yield put(signOutFailure(error))
+    }
+}
+
 export function* onGoogleSignInStart(){
     yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle )
 };
@@ -56,10 +65,15 @@ export function* onCheckUserSession(){
     yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated )
 }
 
+export function* onSignOutStart(){
+    yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut)
+}
+
 export function* userSagas(){
     yield all([
         call(onGoogleSignInStart), 
         call(onEmailSignInStart),
-        call(onCheckUserSession)
+        call(onCheckUserSession),
+        call(onSignOutStart)
     ]);
 };
